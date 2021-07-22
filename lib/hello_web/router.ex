6c036ac2370955291324d7ca3d050e8ab95283bf
Florian Tönjes <skatepainter@yahoo.de>
2021-07-22 22:06:53 +0200

    About to start with cross context dependencies section

--- lib/hello_web/router.ex
+++ lib/hello_web/router.ex
@@ -17,6 +17,8 @@ defmodule HelloWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    resources "/users", UserController
+    resources "/sessions", SessionController, only: [:new, :create, :delete], singleton: true
   end
 
   # Other scopes may use custom stacks.
@@ -37,6 +39,18 @@ defmodule HelloWeb.Router do
     scope "/" do
       pipe_through :browser
       live_dashboard "/dashboard", metrics: HelloWeb.Telemetry
+    end
+  end
+
+  defp authenticate_user(conn, _) do
+    case get_session(conn, :user_id) do
+      nil ->
+        conn
+        |> Phoenix.Controller.put_flash(:error, "Login required")
+        |> Phoenix.Controller.redirect(to: "/")
+        |> halt()
+      user_id ->
+        assign(conn, :current_user, Hello.Accounts.get_user!(user_id))
     end
   end
 end
