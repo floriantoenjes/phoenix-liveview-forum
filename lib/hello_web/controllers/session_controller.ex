959ba2e2e52d5a55e4e064b8d52842e10bdbaced
Florian Tönjes <skatepainter@yahoo.de>
2021-09-17 23:53:20 +0200

    Putting current author into session on login and moved menu to live.html.leex

--- lib/hello_web/controllers/session_controller.ex
+++ lib/hello_web/controllers/session_controller.ex
@@ -10,9 +10,12 @@ defmodule HelloWeb.SessionController do
   def create(conn, %{"user" => %{"email" => email, "password" => password}}) do
     case Accounts.authenticate_by_email_password(email, password) do
       {:ok, user} ->
+        author = Hello.Accounts.get_user!(user.id)
+
         conn
         |> put_flash(:info, "Welcome back!")
         |> put_session(:user_id, user.id)
+        |> put_session(:current_author, author)
         |> configure_session(renew: true)
         |> redirect(to: "/")
       {:error, :unauthorized} ->
--- lib/hello_web/router.ex
+++ lib/hello_web/router.ex
@@ -77,6 +77,5 @@ defmodule HelloWeb.Router do
   defp require_existing_author(conn, _) do
     author = Forum.ensure_author_exists(conn.assigns.current_user)
     assign(conn, :current_author, author)
-    conn = put_session(conn, :current_author, author)
   end
 end
