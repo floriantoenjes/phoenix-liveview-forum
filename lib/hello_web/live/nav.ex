e8fe3c8a3cc0139802282aeb7bf46319b759d1da
Florian Tönjes <skatepainter@yahoo.de>
2021-10-17 10:48:15 +0200

    Removed obsolete code

--- lib/hello_web/live/nav.ex
+++ lib/hello_web/live/nav.ex
@@ -2,8 +2,4 @@ defmodule HelloWeb.NavLive do
   use Phoenix.LiveComponent
   use HelloWeb, :live_view
 
-  def mount(%{"current_author" => author}, socket) do
-    {:ok, socket |> assign(:author, author)}
-  end
-
 end
