c20626095651cfa316f9f23855647ff93d76f731
Florian Tönjes <skatepainter@yahoo.de>
2021-10-18 22:31:01 +0200

    Further implemented notifications

--- lib/hello_web/live/nav.ex
+++ lib/hello_web/live/nav.ex
@@ -2,4 +2,9 @@ defmodule HelloWeb.NavLive do
   use Phoenix.LiveComponent
   use HelloWeb, :live_view
 
+  def mount(socket) do
+    notifications = [Hello.Forum.get_notification!(1)]
+    {:ok, socket |> assign(:notifications, notifications)}
+  end
+
 end
