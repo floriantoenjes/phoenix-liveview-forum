93e1b736970818a1246e0260878bbbbe727ac26c
Florian Toenjes <skatepainter@yahoo.de>
2022-03-02 22:24:17 +0100

    Implemented navigation to last thread page on post creation

--- lib/hello_web/live/thread_detail.ex
+++ lib/hello_web/live/thread_detail.ex
@@ -7,15 +7,18 @@ defmodule HelloWeb.ThreadDetailLive do
   alias Hello.Forum.Post
   alias Hello.Forum.Notification
 
+  defp get_page_count(thread_id) do
+    total_posts = List.first(Forum.get_post_count(thread_id))
+    trunc(Float.ceil(total_posts / 10))
+  end
+
   def mount(%{"id" => board_id, "thread_id" => thread_id}, %{"current_author" => author}, socket) do
     thread = Forum.get_thread!(thread_id)
     posts = Forum.list_posts_paginated(board_id, thread_id, 0)
 
     changeset = Forum.change_post(%Post{})
 
-    total_posts = List.first(Forum.get_post_count(thread_id))
-    page_count = trunc(Float.ceil(total_posts / 10))
-    pages = Enum.to_list(1..page_count)
+    pages = Enum.to_list(1..get_page_count(thread_id))
 
     {:ok, socket |> assign(:author, author)
           |> assign(:board_id, board_id)
@@ -37,12 +40,6 @@ defmodule HelloWeb.ThreadDetailLive do
     {:noreply, socket |> assign(:posts, posts)}
   end
 
-  def handle_event("changePage", %{"page" => page}, socket) do
-    posts = Forum.list_posts_paginated(socket.assigns.board_id, socket.assigns.thread.id, String.to_integer(page))
-
-    {:noreply, socket |> assign(:posts, posts)}
-  end
-
   def handle_event("createPost", %{"post" => post_params}, socket) do
     new_post = Hello.Forum.create_post(socket.assigns.author, socket.assigns.thread, post_params)
 
@@ -50,7 +47,15 @@ defmodule HelloWeb.ThreadDetailLive do
 
     posts = Forum.list_posts_paginated(socket.assigns.board_id, socket.assigns.thread.id, String.to_integer(socket.assigns.current_page) - 1)
 
-    {:noreply, socket |> assign(:thread, Hello.Forum.get_thread!(socket.assigns.thread.id)) |> assign(:posts, posts)}
+    current_page = get_page_count(socket.assigns.thread.id)
+
+    {:noreply, socket
+               |> assign(:thread, Hello.Forum.get_thread!(socket.assigns.thread.id))
+               |> assign(:posts, posts)
+               |> assign(:page, current_page)
+               |> redirect(to: Routes.live_path(socket, HelloWeb.ThreadDetailLive, socket.assigns.board_id, socket.assigns.thread.id)
+                <> "?page=" <> Integer.to_string(current_page))
+    }
   end
 
   def handle_event("subscribe", _params, socket) do
